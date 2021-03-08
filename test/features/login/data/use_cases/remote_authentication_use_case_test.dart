b9faafb0c174c66cc5dc5d6bd0afbf01e09aa6b7
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-03-07 23:23:38 -0300

    feat: add test Should call HttpClient with correct values

--- test/features/login/data/use_cases/remote_authentication_use_case_test.dart
+++ test/features/login/data/use_cases/remote_authentication_use_case_test.dart
@@ -4,7 +4,7 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 abstract class HttpClient {
-  Future<void> request({@required String url});
+  Future<void> request({@required String url, @required String method});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -16,18 +16,21 @@ class RemoteAuthenticationUseCase {
   RemoteAuthenticationUseCase({@required this.httpClient, @required this.url});
 
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 void main() {
-  test('Should call HttpClient with correct URL', () async {
+  test('Should call HttpClient with correct values', () async {
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
     final sut = RemoteAuthenticationUseCase(httpClient: httpClient, url: url);
 
     await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+    ));
   });
 }
