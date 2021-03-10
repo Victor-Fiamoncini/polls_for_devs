717e73fa5f1e50c9cdf434ec0fa241e729a1c1b9
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-03-10 09:19:48 -0300

    chore: add main & ui layers

---
+++ lib/main/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:polls_for_devs/ui/components/app.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/ui/components/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:polls_for_devs/ui/pages/login_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
