b11e5d9d2d80f5261cb63acc9f407f64e89c3b8a
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-03-29 00:10:28 -0300

    feat: add test Should return error if value is empty

--- test/validation/validators/required_field_validator_test.dart
+++ test/validation/validators/required_field_validator_test.dart
@@ -14,16 +14,24 @@ class RequiredFieldValidator implements FieldValidator {
 
   @override
   String validate(String value) {
-    return null;
+    return value.isEmpty ? 'Campo obrigatório.' : null;
   }
 }
 
 void main() {
-  test('Should return null if valid is not empty', () {
+  test('Should return null if value is not empty', () {
     final sut = RequiredFieldValidator('any_field');
 
     final error = sut.validate('any_value');
 
     expect(error, null);
+  });
+
+  test('Should return error if value is empty', () {
+    final sut = RequiredFieldValidator('any_field');
+
+    final error = sut.validate('');
+
+    expect(error, 'Campo obrigatório.');
   });
 }
