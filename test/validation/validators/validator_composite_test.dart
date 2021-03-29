8a6208c4ca7b6aca744c794319ce866b222a5b15
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-03-29 01:02:25 -0300

    feat: add test Should return the first error found

--- test/validation/validators/validator_composite_test.dart
+++ test/validation/validators/validator_composite_test.dart
@@ -11,7 +11,17 @@ class ValidatorComposite implements Validation {
 
   @override
   String validate({@required String field, @required String value}) {
-    return null;
+    String error;
+
+    for (final validator in validators) {
+      error = validator.validate(value);
+
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+
+    return error;
   }
 }
 
@@ -19,24 +29,53 @@ class FieldValidatorSpy extends Mock implements FieldValidator {}
 
 void main() {
   ValidatorComposite sut;
+  FieldValidatorSpy validator01;
+  FieldValidatorSpy validator02;
+  FieldValidatorSpy validator03;
+
+  void mockValidation01(String error) {
+    when(validator01.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation02(String error) {
+    when(validator02.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation03(String error) {
+    when(validator03.validate(any)).thenReturn(error);
+  }
 
   setUp(() {
-    sut = ValidatorComposite([]);
+    validator01 = FieldValidatorSpy();
+    when(validator01.field).thenReturn('any_field');
+    mockValidation01(null);
+
+    validator02 = FieldValidatorSpy();
+    when(validator02.field).thenReturn('any_field');
+    mockValidation02(null);
+
+    validator03 = FieldValidatorSpy();
+    when(validator03.field).thenReturn('other_field');
+    mockValidation03(null);
+
+    sut = ValidatorComposite([validator01, validator02, validator03]);
   });
 
   test('Should return null if all validators returns null or empty', () {
-    final validator01 = FieldValidatorSpy();
-    when(validator01.field).thenReturn('any_field');
-    when(validator01.validate(any)).thenReturn(null);
-
-    final validator02 = FieldValidatorSpy();
-    when(validator02.field).thenReturn('any_field');
-    when(validator02.validate(any)).thenReturn('');
-
-    sut = ValidatorComposite([validator01, validator02]);
+    mockValidation02('');
 
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
+  });
+
+  test('Should return the first error', () {
+    mockValidation01('error_01');
+    mockValidation02('error_02');
+    mockValidation03('error_03');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, 'error_01');
   });
 }
