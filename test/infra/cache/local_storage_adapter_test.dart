f7dceb6b809ceae8768cfbca3a3e646f8fcd858c
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-04-03 20:50:19 -0300

    feat: add test Should call save secure cache with correct values

---
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -0,0 +1,42 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:polls_for_devs/data/cache/save_secure_cache_storage.dart';
+import 'package:test/test.dart';
+
+class LocalStorageAdapter implements SaveSecureCacheStorage {
+  final FlutterSecureStorage secureStorage;
+
+  LocalStorageAdapter({@required this.secureStorage});
+
+  @override
+  Future<void> saveSecure({
+    @required String key,
+    @required String value,
+  }) async {
+    await secureStorage.write(key: key, value: value);
+  }
+}
+
+class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
+
+void main() {
+  LocalStorageAdapter sut;
+  FlutterSecureStorageSpy secureStorage;
+  String key;
+  String value;
+
+  setUp(() {
+    secureStorage = FlutterSecureStorageSpy();
+    sut = LocalStorageAdapter(secureStorage: secureStorage);
+    key = faker.lorem.word();
+    value = faker.guid.guid();
+  });
+
+  test('Should call save secure cache with correct values', () async {
+    await sut.saveSecure(key: key, value: value);
+
+    verify(secureStorage.write(key: key, value: value)).called(1);
+  });
+}
